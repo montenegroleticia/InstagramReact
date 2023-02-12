9cacc6c816e6d48f6247fcac098600695ed6d006
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-02-12 12:19:44 -0300

    Usando useState no usuário

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,11 +1,28 @@
-export default function Usuario(){
-    return (
-        <div className="login">
-          <img src="./midia/wandinha.jpeg" alt="wandinha" />
-          <div className="nome-login">
-            <p className="negrito">wandinha</p>
-            <ion-icon name="pencil"></ion-icon>
-          </div>
-        </div>
-    );
+import { useState } from "react";
+
+export default function Usuario(props) {
+  const [nome, setNome] = useState("wandinha");
+  const [image, setImage] = useState("./midia/wandinha.jpeg");
+
+  function nomeUser() {
+    const novoNome = prompt("Insira seu nome:").trim();
+    if (nome.length > 0) {
+      setNome(novoNome);
+    }
+  }
+
+  function imageUser() {
+    const novaImage = prompt("Insira imagem:");
+    setImage(novaImage);
+  }
+
+  return (
+    <div className="login">
+      <img onClick={imageUser} src={image} alt={nome} />
+      <div className="nome-login">
+        <p className="negrito">{nome}</p>
+        <ion-icon onClick={nomeUser} name="pencil"></ion-icon>
+      </div>
+    </div>
+  );
 }
