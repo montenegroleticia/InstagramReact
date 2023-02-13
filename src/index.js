bb387f335c58a656b9af322dc0547b0784ea044e
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-02-13 13:12:50 -0300

    Removido comentários

--- src/index.js
+++ src/index.js
@@ -4,6 +4,3 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<App />);
-
-//import ReactDOM from "react-dom";
-//ReactDOM.render(<App />, document.querySelector(".root"));
